eecbac9e8aa9160e86cfc95277d94b54f01623b1
Rony Xavier <rx294@nyu.edu>
2018-02-15 14:52:59 -0500

    updates
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

--- test/integration/dev/build/aws.tf
+++ test/integration/dev/build/aws.tf
@@ -6,7 +6,7 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
-  version    = "~> 1.4"
+  version    = "~> 1.1"
 }
 
 data "aws_caller_identity" "creds" {}
@@ -15,10 +15,12 @@ output "aws_account_id" {
   value = "${data.aws_caller_identity.creds.account_id}"
 }
 
-data "aws_region" "region" {
-  current = true
-}
+# data "aws_region" "region" {
+#   current = true
+# }
 
-output "aws_region" {
-  value = "${data.aws_region.region.name}"
-}
+
+# output "aws_region" {
+#   value = "${data.aws_region.region.name}"
+# }
+
